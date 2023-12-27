76093748c9f1eec78a62ffeb74e8adfad7064e7a
Daniel Zhang <z331317@gmail.com>
2023-12-27 11:30:32 -0500

    fixing async operations

---
+++ .ipynb_checkpoints/chatbot-checkpoint.py
@@ -0,0 +1,34 @@
+ 
+import os 
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key=os.getenv("OPENAI_API_KEY")
+#venv\Scripts\activate
+#source venv/bin/activate
+#messages=[{"role": "system", "content": "You are to return a conversation starter based on the messages you're given. Only return the #conversation starter. Make it relevent to the conversation."}]
+
+
+# while True:
+#   user_input=input("Say something: ")
+
+#   if user_input=='exit':
+#     print("exiting program.")
+#     break
+
+#   messages.append({"role": "user", "content": user_input})
+
+
+# completion = openai.chat.completions.create(
+#   model="gpt-3.5-turbo",
+#   messages=messages
+# )
+
+def generateResponse(messages):
+  completion = openai.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=messages
+  )
+  return completion.choices[0].message
+ 
---
+++ chatbot.py
@@ -0,0 +1,37 @@
+ 
+import os 
+import openai
+from dotenv import load_dotenv
+import threading
+
+load_dotenv()
+openai.api_key=os.getenv("OPENAI_API_KEY")
+#venv\Scripts\activate
+#source venv/bin/activate
+#messages=[{"role": "system", "content": "You are to return a conversation starter based on the messages you're given. Only return the #conversation starter. Make it relevent to the conversation."}]
+
+
+# while True:
+#   user_input=input("Say something: ")
+
+#   if user_input=='exit':
+#     print("exiting program.")
+#     break
+
+#   messages.append({"role": "user", "content": user_input})
+
+
+# completion = openai.chat.completions.create(
+#   model="gpt-3.5-turbo",
+#   messages=messages
+# )
+
+def generateResponse(messages):
+  print("GENERATING") 
+  completion =  openai.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=messages
+  ) 
+  print("CHatGPT REsposen: "+completion.choices[0].message)
+  return completion.choices[0].message
+ 
